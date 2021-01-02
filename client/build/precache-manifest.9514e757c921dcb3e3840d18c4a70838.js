44e36b3243e1b49dc9c04e6a0ac0e5dc47f0c0f5
Christian Castillo <ccastilloc356@live.com>
2021-01-01 18:36:37 -0800

    working app locally

---
+++ client/build/precache-manifest.9514e757c921dcb3e3840d18c4a70838.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb7f1b9d1b0bb8702978",
+    "url": "/static/css/main.e4e26e58.chunk.css"
+  },
+  {
+    "revision": "cb7f1b9d1b0bb8702978",
+    "url": "/static/js/main.e1a0af79.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b01c46d6e98817f4adfc",
+    "url": "/static/css/2.66821e88.chunk.css"
+  },
+  {
+    "revision": "b01c46d6e98817f4adfc",
+    "url": "/static/js/2.947a4c27.chunk.js"
+  },
+  {
+    "revision": "f368baac251b582ef820524fc0a22820",
+    "url": "/index.html"
+  }
+];
